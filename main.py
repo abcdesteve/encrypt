4b6bdaab8edad3c00353fc87fc305341bfdc9d4a
abcdesteve <72078508+abcdesteve@users.noreply.github.com>
2023-12-17 01:07:06 +0800

    v1.4

--- main.py
+++ main.py
@@ -1,50 +1,55 @@
-import tkinter
-import random
+import sys
 import json
-import tkinter.filedialog
-def lock(code):
-    dic = {}
-    key=[]
-    with open(tkinter.filedialog.askopenfilename(filetypes=[('All File', '*')]), 'r', encoding=code)as read:
-        data = read.read()
-    allkey=''
-    for temp in data:
-        if temp not in allkey:
-            allkey+=temp
-    temp = 0
-    for temp in allkey:
-        key.append(temp)
-    temp = 0
-    for temp in allkey:
-        dic[temp] = random.choice(key)
-        key.remove(dic[temp])   
-    jsondata=''
-    temp = 0
-    for temp in data:
-        jsondata=jsondata+dic[temp]
-    dic['data']=jsondata
-    with open(tkinter.filedialog.asksaveasfilename(filetypes=[('神龙加密文件', '*.sljm')], initialfile='.sljm'), 'w', encoding='utf-8')as write:
-        json.dump(dic, write)
-def unlock(code):
-    dic = {}
-    data=''
-    with open(tkinter.filedialog.askopenfilename(filetypes=[('神龙加密文件', '*.sljm')], initialfile='.sljm'), 'r', encoding='utf-8')as read:
-              temp = json.load(read)
-    jsondata = temp.pop('data')
-    for key in temp:
-        dic[temp[key]]=key
-    temp = 0
-    for temp in jsondata:
-        data=data+dic[temp]
-    with open(tkinter.filedialog.asksaveasfilename(filetypes=[('All File', '*')]), 'w', encoding=code)as write:
-        write.write(data)
-win = tkinter.Tk()
-win.attributes('-topmost',True)
-win.title('神龙加密v1.3')
-text = tkinter.Label(win, text='          ---欢迎使用 神龙加密v1.3---          \nby abcdesteve\n')
-text.pack()
-b1 = tkinter.Button(win, text='加密', command=lambda: lock(input('打开文件格式?')))
-b1.pack(side='left')
-b2 = tkinter.Button(win, text='解密', command=lambda: unlock(input('保存文件格式?')))
-b2.pack(side='right')
-win.mainloop()
+import random
+import codecs
+from PySide2.QtWidgets import *
+from ui_gui import Ui_gui
+
+
+class Main(QMainWindow, Ui_gui):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.lock_bottom.clicked.connect(self.lock)
+        self.unlock_bottom.clicked.connect(self.unlock)
+
+        self.show()
+
+    def lock(self):
+        with codecs.open(QFileDialog.getOpenFileName(caption='打开文件')[0], 'r', encoding=self.open_format.currentText())as path:
+            file = path.read()
+        keys, values = [], []
+        for temp in file:
+            if temp not in keys:
+                keys.append(temp)
+                values.append(temp)
+        data = {}
+        random.shuffle(values)
+        for index in range(len(keys)):
+            data[keys[index]] = values[index]
+        code = ''
+        for temp in file:
+            code += data[temp]
+        data['data'] = code
+        with codecs.open(QFileDialog.getSaveFileName(caption='保存文件', filter='(*.sljm)')[0], 'w', encoding='utf-8')as path:
+            json.dump(data, path)
+
+    def unlock(self):
+        with codecs.open(QFileDialog.getOpenFileName(caption='打开文件', filter='(*.sljm)')[0], 'r', encoding='utf-8')as path:
+            file = json.load(path)
+        code = file.pop('data')
+        data = {}
+        for index in range(len(file)):
+            data[list(file.values())[index]] = list(file.keys())[index]
+        text = ''
+        for temp in code:
+            text += data[temp]
+        with codecs.open(QFileDialog.getSaveFileName(caption='保存文件')[0], 'w', encoding=self.save_format.currentText())as path:
+            path.write(text)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Main()
+    sys.exit(app.exec_())
